e76ff45fb7909bedd38c21e6122f604334ac25b7
stephan.struckmann <stephan.struckmann@uni-greifswald.de>
2024-04-29 14:42:40 +0200

    pull the correct base image before building

--- make.sh
+++ make.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # ensure the newst base image
-docker pull r-base
+docker pull rocker/verse:latest-daily
 
 # build the image
 docker build -t dataquality/dataquier-2-shiny --progress plain .
